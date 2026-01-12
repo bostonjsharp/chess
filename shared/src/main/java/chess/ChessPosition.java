1bb0b6dc7d3506eede6083a8c88bd2aa5d575ba0
Boston Sharp <62575690+bostonjsharp@users.noreply.github.com>
2026-01-11 18:37:27 -0700

    Implemented Postion and Board
    Added the overrides to Position and got board going (mostly?). Come back to check board after implementing ChessPiece

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -9,6 +11,20 @@ package chess;
 public class ChessPosition {
     private int row = 1;
     private int column = 1;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && column == that.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
 
     public ChessPosition(int row, int col) {
         this.row = row;
