5cee28540048c476909fedb632d7dced315a1d5d
Boston Sharp <62575690+bostonjsharp@users.noreply.github.com>
2026-01-14 23:32:00 -0700

    Moves pseudocode
    Added pseudocode to keep track of everything needed for setting moves.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -71,7 +71,37 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
        ChessPiece piece = board.getPiece(myPosition);
        if (piece.getPieceType() == PieceType.BISHOP) {
+           //slides in diagonal directions
+           //stops if hits edges, friendlies, or captures piece
            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+       }
+       if (piece.getPieceType() == PieceType.KING) {
+           //can only go one space in any direction
+           //stopped by edges, friendlies.
+           //uses offsets...?
+
+       }
+       if (piece.getPieceType() == PieceType.PAWN) {
+           //can only move forward unless diagonal has enemy
+           //forward two spaces only on first move
+           //en passant?
+           //promoted to different piece if it reaches the end of the board
+
+       }
+       if (piece.getPieceType() == PieceType.ROOK) {
+           //slides in cardinal directions
+           //stops at edges, friendlies, caputres piece
+
+       }
+       if (piece.getPieceType() == PieceType.KNIGHT) {
+           //2 spaces, then three spaces (L shape)
+           //jumps over pieces
+           //offsets as well...?
+
+       }
+       if (piece.getPieceType() == PieceType.QUEEN) {
+           //rook and bishop moves + king moves
+
        }
 
         return List.of();
