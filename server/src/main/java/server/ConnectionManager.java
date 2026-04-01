c5be2fe4f7c72f1f4db8a136153ccfe103213d87
Boston Sharp <62575690+bostonjsharp@users.noreply.github.com>
2026-03-31 23:28:21 -0600

    Connection manager done

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -1,4 +1,21 @@
 package server;
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
+    private final Map<Integer, Set<ClientConnection>> connections = new ConcurrentHashMap<>();
+
+    public void add(Integer gameID, String username, Session session) {
+        connections.computeIfAbsent(gameID, id -> ConcurrentHashMap.newKeySet()).add(new ClientConnection(username, session));
+    }
+
+    public void remove(Integer gameID, Session session) {
+        var gameConnections = connections.get(gameID);
+        if(gameConnections != null){
+            gameConnections.removeIf(connection -> connection.session().equals(session));
+        }
+    }
 }
