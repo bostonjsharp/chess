18514cdc30353c96336302bd4e17e67b4b98efdd
Boston Sharp <62575690+bostonjsharp@users.noreply.github.com>
2026-04-04 11:04:17 -0600

    updated websocket

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -2,10 +2,16 @@ package server;
 
 import com.google.gson.Gson;
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
+import model.GameData;
+import model.AuthData;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsMessageContext;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 
 public class WebSocketHandler {
     private final AuthDAO authDAO;
@@ -26,7 +32,7 @@ public class WebSocketHandler {
                 connect(context, command);
             }
         } catch (Exception e) {
-            System.out.println("WebSocket message error: " + e.getMessage());
+            sendError(context, e.getMessage());
         }
     }
 
@@ -34,6 +40,53 @@ public class WebSocketHandler {
         System.out.println("Websocket Closed!");
     }
     private void connect(WsMessageContext context, UserGameCommand command){
-        System.out.println("CONNECT received");
+        try{
+            AuthData authData = authDAO.getAuth(command.getAuthToken());
+            if(authData == null){
+                sendError(context, "invalid auth token");
+                return;
+            }
+            GameData gameData = gameDAO.getGame(command.getGameID());
+            if(gameData == null) {
+                sendError(context,"Game Not Found...");
+                return;
+            }
+
+            String username = authData.username();
+            String role = getRole(gameData,username);
+            connectionManager.add(command.getGameID(), username, context.session);
+            LoadGameMessage loadGameMessage = new LoadGameMessage(gameData.game());
+            context.send(gson.toJson(loadGameMessage));
+            String notification;
+
+            if(role.equals("WHITE")){
+                notification = username + " joined the game as White";
+            } else if (role.equals("BLACK")){
+                notification = username + " joined the game as Black";
+            } else {
+                notification = username + " joined the game as an observer";
+            }
+            NotificationMessage notificationMessage = new NotificationMessage(notification);
+            connectionManager.broadcastExceptRoot(command.getGameID(), context.session, gson.toJson(notification));
+        } catch (DataAccessException e) {
+            sendError(context, e.getMessage());
+        } catch (Exception e) {
+            sendError(context, e.getMessage());
+        }
+    }
+
+    private String getRole(GameData gameData, String username){
+        if(username.equals(gameData.whiteUsername())){
+            return "WHITE";
+        }
+        if (username.equals(gameData.blackUsername())){
+            return "BLACK";
+        }
+        return "OBSERVER";
+    }
+
+    private void sendError(WsMessageContext context, String error){
+        ErrorMessage errorMessage = new ErrorMessage(error);
+        context.send(gson.toJson(errorMessage));
     }
 }
